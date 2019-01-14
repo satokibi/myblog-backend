9b99f4526e26cf98c7f4e0e2215e9082933f3f90
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-01-15 00:50:53 +0900

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root 'sessions#new'
+  root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get    'login',  to: 'sessions#new'
