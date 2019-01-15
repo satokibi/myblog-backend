858933b1ac6121716f1f03eebb54e2eee4fcd6eb
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-01-15 17:08:47 +0900

    finish layout category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -29,7 +29,10 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html {
+           flash[:success] = "Category was successfully created."
+           redirect_to( categories_url )
+        }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -43,7 +46,10 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html {
+           flash[:success] = "Category was successfully updated."
+           redirect_to( @category )
+        }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
@@ -57,7 +63,10 @@ class CategoriesController < ApplicationController
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html {
+        flash[:success] = "Category was successfully destroyed."
+        redirect_to( categories_url )
+      }
       format.json { head :no_content }
     end
   end
