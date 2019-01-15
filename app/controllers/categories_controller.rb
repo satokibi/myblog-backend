8c729b449bf3a15c4dfa6d64c169a10fbf27609a
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-01-15 16:50:09 +0900

    finish layout of cotegories/index.html.erb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action( :signed_in_user )
 
   # GET /categories
   # GET /categories.json
